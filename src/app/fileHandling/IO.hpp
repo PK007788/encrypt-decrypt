5f0bcdf56e1ed722669e9200a2fb0f0c0f287a0e
Prajnan Kumar Sarma <prajnankumarsarma102030@gmail.com>
2026-06-07 19:12:38 +0530

    Fix formatting and add missing include for IO class in IO.hpp

--- src/app/fileHandling/IO.hpp
+++ src/app/fileHandling/IO.hpp
@@ -3,10 +3,17 @@
 
 #include<fstream>
 #include<string>
+#include<iostream>
 
 class IO{
     public:
-        IO(const std:: str)
-}
+        // constructor
+        IO(const std::string &file_path );  // here we have used constant as file path never changes and we are storing filepath by reference
+        // destructor
+        ~IO();
+        std::fstream getFileStream(); 
+    };
+
+
 
 #endif
